38266d9d8f9f6d9951b914ba730fcc392d996cf3
gojaime <gojaime@up.edu.ph>
2024-05-31 18:21:01 +0800

    gpt

--- testing.c
+++ testing.c
@@ -23,33 +23,32 @@ ssize_t read_all(int socket, void *buffer, size_t size) {
             return -1;
         }
         total_read += bytes_read;
-
     }
 
     return total_read;
 }
 
+float pearson_cor(float *x, float *y, int n) {
+    float sum_x = 0, sum_y = 0, sum_xy = 0;
+    float sum_x2 = 0, sum_y2 = 0;
 
-// float pearson_cor(int arr[], int y[], int n){
-//     float x=0;
-//     float x2=0;
-//     float y=0;
-//     float y2=0;
-//     float xy=0;
-//     for (int i=0;i<n;n++){
-//         x += arr[i];
-//         x2 += arr[i] * arr[i];
-//         y += y[i];
-//         y2 += y[i] * y[i];
-//         xy += x[i] * y[i];
-//     }
+    for (int i = 0; i < n; i++) {
+        sum_x += x[i];
+        sum_y += y[i];
+        sum_xy += x[i] * y[i];
+        sum_x2 += x[i] * x[i];
+        sum_y2 += y[i] * y[i];
+    }
 
-//     return ((m * xy) - (x * y)) / (m * x2) - (x);
-// }
+    float numerator = n * sum_xy - sum_x * sum_y;
+    float denominator = sqrt((n * sum_x2 - sum_x * sum_x) * (n * sum_y2 - sum_y * sum_y));
+    if (denominator == 0) return 0; // Prevent division by zero
+    return numerator / denominator;
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 4) {
-        fprintf(stderr, "Usage: %s n p s \n", argv[0]);
+        fprintf(stderr, "Usage: %s n port s\n", argv[0]);
         return EXIT_FAILURE;
     }
 
@@ -59,33 +58,33 @@ int main(int argc, char *argv[]) {
 
     if (s == 0) {
         // master mode
-        FILE* fp;
+        FILE *fp;
         int bufferLength = 255;
         char buffer[bufferLength];
 
         fp = fopen("slaves.txt", "r");
-        if (fp == NULL)
-            exit(EXIT_FAILURE);
+        if (fp == NULL) error("Failed to open slaves.txt");
 
         // fetch t from first line
-        fgets(buffer, bufferLength, fp);
+        if (fgets(buffer, bufferLength, fp) == NULL) error("Failed to read from slaves.txt");
         buffer[strcspn(buffer, "\n")] = 0;
         int t = atoi(buffer);
-        printf("\nT = %i\n", t);
+        printf("T = %i\n", t);
 
         // initialize array to split
-        float *array;
-        array = (float *)malloc(n * n * sizeof(float));
+        float *array = (float *)malloc(n * n * sizeof(float));
+        if (array == NULL) error("Failed to allocate memory for array");
 
         for (int i = 0; i < n * n; i++) {
             array[i] = (rand() % 99) + 1;
         }
 
         // initialize y vector
-        float *y;
-        y = (float *)malloc(n * sizeof(float));
-        for (int i=0; i<n; i++) {
-            y[i] = (random() % 99) + 1;
+        float *y = (float *)malloc(n * sizeof(float));
+        if (y == NULL) error("Failed to allocate memory for y vector");
+
+        for (int i = 0; i < n; i++) {
+            y[i] = (rand() % 99) + 1;
         }
 
         // initialize index
@@ -96,7 +95,7 @@ int main(int argc, char *argv[]) {
 
         // loop through slave IPs
         while (fgets(buffer, bufferLength, fp)) {
-            printf("\nSending to %s\n", buffer);
+            printf("Sending to %s\n", buffer);
 
             // remove trailing newline
             buffer[strcspn(buffer, "\n")] = 0;
@@ -107,97 +106,75 @@ int main(int argc, char *argv[]) {
             int sock = 0;
             struct sockaddr_in serv_addr;
             if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-                printf("\n Socket creation error \n");
-                return -1;
+                error("Socket creation error");
             }
 
             serv_addr.sin_family = AF_INET;
             serv_addr.sin_port = htons(atoi(port_str));
 
             if (inet_pton(AF_INET, ip_port, &serv_addr.sin_addr) <= 0) {
-                printf("\nInvalid address/ Address not supported \n");
-                return -1;
+                error("Invalid address/ Address not supported");
             }
 
             if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-                printf("\nConnection Failed \n");
-                return -1;
+                error("Connection Failed");
             }
-            float *rows;
-            rows = (float *)malloc(sizeof(float));
 
-
-            rows[0] = threadIndex == t? (chunk_size)/n : n/t;
-            // printf("\nRows = %d\n",rows[0]);
+            float rows = (threadIndex == t) ? (chunk_size) / n : n / t;
 
             // initialize submatrix
-            float *submatrix;
-            submatrix = (float *)malloc(chunk_size * sizeof(float));
+            float *submatrix = (float *)malloc(chunk_size * sizeof(float));
+            if (submatrix == NULL) error("Failed to allocate memory for submatrix");
+
             int currIndex = 0;
             for (int i = startingIndex; i < endingIndex; i++) {
-                submatrix[currIndex] = array[i];
-                currIndex++;
+                submatrix[currIndex++] = array[i];
             }
+
             startingIndex += chunk_size;
-            if (threadIndex == t-1) {
+            if (threadIndex == t - 1) {
                 endingIndex = n * n;
-                chunk_size = (n*n) - ((threadIndex) * chunk_size);
-
-
-                // printf("\n last index %d\n", endingIndex);
+                chunk_size = (n * n) - ((threadIndex) * chunk_size);
             } else {
                 endingIndex += chunk_size;
-                // printf("\n not yet\n");
             }
 
-
             // time_before
-            clock_t t;
-            t = clock();
+            clock_t t_start = clock();
 
             // send rows
-            send(sock, rows, sizeof(float), 0);
+            send(sock, &rows, sizeof(float), 0);
             // send submatrix
             send(sock, submatrix, chunk_size * sizeof(float), 0);
             printf("Array sent\n");
             // send y vector
-            send(sock, y, n * sizeof(float),0);
-
+            send(sock, y, n * sizeof(float), 0);
 
             // wait for ack from slave
             char ack_msg[4] = "ack";
             char recv_ack[4];
             recv(sock, recv_ack, 4, 0);
             if (strcmp(recv_ack, ack_msg) != 0) {
-                printf("Error: Unexpected ack message from slave\n");
-                return -1;
+                error("Unexpected ack message from slave");
+            } else {
+                printf("Received ack from %s\n", buffer);
             }
-            else printf("Received ack from %s\n",buffer);
 
             // wait for r vector
-
+            // Code for receiving the r vector from slave can be added here
 
             threadIndex++;
             free(submatrix);
             close(sock);
+
+            // time_after
+            double time_taken = ((double)(clock() - t_start)) / CLOCKS_PER_SEC;
+            printf("Elapsed time: %f seconds\n", time_taken);
         }
 
-        // time_after
-        t = clock() - t;
-
-        // show sent array
-
-        // for(int i=0; i<n*n; i++){
-        //     if(i%n==0){
-        //         printf("\n");
-        //     }
-        //     printf("%d ", array[i]);
-        // }
-
-        double time_taken = ((double)t)/CLOCKS_PER_SEC;
-        printf("\nElapsed time: %f seconds\n", time_taken);
-
         free(array);
+        free(y);
+        fclose(fp);
 
     } else if (s == 1) {
         // slave mode
@@ -205,16 +182,13 @@ int main(int argc, char *argv[]) {
         struct sockaddr_in address;
         int opt = 1;
         int addrlen = sizeof(address);
-        
 
         if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-            perror("socket failed");
-            exit(EXIT_FAILURE);
+            error("Socket creation failed");
         }
 
         if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-            perror("setsockopt");
-            exit(EXIT_FAILURE);
+            error("setsockopt failed");
         }
 
         address.sin_family = AF_INET;
@@ -222,111 +196,66 @@ int main(int argc, char *argv[]) {
         address.sin_port = htons(port);
 
         if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-            perror("bind failed");
-            exit(EXIT_FAILURE);
+            error("Bind failed");
         }
 
-        printf("Listening on port %d\n",port);
+        printf("Listening on port %d\n", port);
 
         if (listen(server_fd, 3) < 0) {
-            perror("listen");
-            exit(EXIT_FAILURE);
+            error("Listen failed");
         }
 
         if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-            perror("accept");
-            exit(EXIT_FAILURE);
+            error("Accept failed");
         }
-        // receive rows
-        float *rows;
-        rows = (float *)malloc(sizeof(float));
-        read_all(new_socket, rows, sizeof(float));
-        printf("Rows: %f\n", rows[0]);
-        int chunk_size = rows[0] * n;
 
-        // receive vector
-        float *y_vector;
-        y_vector = (float *)malloc(chunk_size * sizeof(float));
-        read_all(new_socket,y_vector, n * sizeof(float));
+        // receive rows
+        float rows;
+        read_all(new_socket, &rows, sizeof(float));
+        printf("Rows: %f\n", rows);
+        int chunk_size = rows * n;
+
+        // receive y vector
+        float *y_vector = (float *)malloc(n * sizeof(float));
+        if (y_vector == NULL) error("Failed to allocate memory for y vector");
+        read_all(new_socket, y_vector, n * sizeof(float));
 
         // receive submatrix
-        float *buffer;
-        buffer = (float *)malloc(chunk_size * sizeof(float));
+        float *buffer = (float *)malloc(chunk_size * sizeof(float));
+        if (buffer == NULL) error("Failed to allocate memory for buffer");
         read_all(new_socket, buffer, chunk_size * sizeof(float));
-        
-
-        // show received array
-        // printf("Array received: \n");
-        // for (int i = 0; i < chunk_size; i++) {
-        //     if (i % n == 0) {
-        //         printf("\n");
-        //     }
-        //     printf("%f ", buffer[i]);
-        // }
-        // printf("\n");
 
         // send ack back to master
         send(new_socket, "ack", 4, 0);
-
-        printf("\nSent ack to master\n");
+        printf("Sent ack to master\n");
 
         // time_before
-        clock_t t;
-        t = clock();
+        clock_t t_start = clock();
 
-        //pearson cor
-        float x=0;
-        float x2=0;
-        float y=0;
-        float y2=0;
-        float xy=0;
+        // calculate Pearson correlation for each row
+        float *correlation_results = (float *)malloc(rows * sizeof(float));
+        if (correlation_results == NULL) error("Failed to allocate memory for correlation results");
 
-        for(int i=0;i<n;i++){
-            y += y_vector[i];
-            y2 += y_vector[i];
+        for (int i = 0; i < rows; i++) {
+            correlation_results[i] = pearson_cor(&buffer[i * n], y_vector, n);
         }
 
-        // for (int i=0;i<n;n++){
-        //     x += arr[i];
-        //     x2 += arr[i] * arr[i];
-        //     y += y[i];
-        //     y2 += y[i] * y[i];
-        //     xy += x[i] * y[i];
-        // }
-
-        // return ((n * xy) - (x * y)) / sqrt(((n * x2) - (x*x)) * ((m * y2) - y2));
-        int yIndex = 0;
-        for (int i = 0; i < chunk_size; i++) {
-
-            if (i % n == 0 && i != 0) {
-                x=0;
-                x2=0;
-                xy=0;
-                y_vector[yIndex] = ((n * xy) - (x * y)) / sqrt(((n * x2) - (x*x)) * ((n * y2) - y2));
-                // printf("\n");
-            }
-            x += buffer[i];
-            x2 += buffer[i] * buffer[i];
-            xy += buffer[i] * y_vector[yIndex];
-            yIndex++;
-            // printf("%f ", buffer[i]);
-        }
-
-        // send vector
-        
-
+        // send correlation results back to master
+        send(new_socket, correlation_results, rows * sizeof(float), 0);
 
         // time_after
-        t = clock() - t;
+        double time_taken = ((double)(clock() - t_start)) / CLOCKS_PER_SEC;
+        printf("Elapsed time: %f seconds\n", time_taken);
 
+        // clean up
+        free(y_vector);
+        free(buffer);
+        free(correlation_results);
         close(new_socket);
         close(server_fd);
 
-        double time_taken = ((double)t)/CLOCKS_PER_SEC;
-        printf("\nElapsed time: %fs\n", time_taken);
         printf("Press enter to exit...\n");
-        char dummy;
-        scanf("%c", &dummy);
+        getchar();
     } else {
         fprintf(stderr, "Invalid value for s. Use 0 for master and 1 for slave.\n");
         return EXIT_FAILURE;
